f819f7ee9c9dac46fe80bafce49142d9380f798d
abdalla aboaziz <abdallaaboaziz@gmail.com>
2025-11-27 22:16:30 +0200

    Add project files.

---
+++ EFCoreLoading Data/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace EFCoreLoading_Data
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
